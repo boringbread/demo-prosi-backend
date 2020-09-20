d1dd921a431a91b136be18437cfdc0b3cc278cb9
obolsuro <obolsuro@gmail.com>
2020-09-20 22:14:27 +0700

    nambahin html spesial chars
    html Spesial chars

--- application/controllers/WebsiteRestController.php
+++ application/controllers/WebsiteRestController.php
@@ -1,24 +1,29 @@
 
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 header('Access-Control-Allow-Origin: *');
-if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-	header('Access-Control-Allow-Headers: Content-Type');
-	exit;
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    exit;
 }
 //required for REST API
 require(APPPATH . '/libraries/REST_Controller.php');
 require APPPATH . 'libraries/Format.php';
+
 use Restserver\Libraries\REST_Controller;
-class WebsiteRestController extends REST_Controller {
-	
-	function __construct() {
+
+class WebsiteRestController extends REST_Controller
+{
+
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('user_model', 'user');
     }
-	
-	function getUser_get($ID) {
+
+    function getUser_get($ID)
+    {
         $users = $this->user->getUserData($ID);
         if ($users) {
             $this->response($users, 200);
@@ -27,7 +32,8 @@ class WebsiteRestController extends REST_Controller {
         }
     }
 
-    function getAllUser_get() {
+    function getAllUser_get()
+    {
         $users = $this->user->getALLUserData();
         if ($users) {
             $this->response($users, 200);
@@ -36,19 +42,20 @@ class WebsiteRestController extends REST_Controller {
         }
     }
 
-    function login_post(){
-        $username = $this->post('Username');
-        $password = $this->post('Password');
-
+    function login_post()
+    {
+        $username = htmlspecialchars($this->post('Username'));
+        $password = htmlspecialchars($this->post('Password'));
         $result = $this->user->login($username, $password);
         if ($result) {
-            $this->response($users, 200);
+            $this->response($result, 200);
         } else {
             $this->response(array(), 401);
         }
     }
 
-    function insertData_post(){
+    function insertData_post()
+    {
         $username = $this->post('Username');
         $password = $this->post('Password');
 
@@ -60,7 +67,8 @@ class WebsiteRestController extends REST_Controller {
         }
     }
 
-    function deleteUser_delete($id){
+    function deleteUser_delete($id)
+    {
         $result = $this->user->deleteUser($id);
         if ($result === FALSE) {
             $this->response(array('status' => 'failed'));
@@ -69,17 +77,17 @@ class WebsiteRestController extends REST_Controller {
         }
     }
 
-    function update_put(){
+    function update_put()
+    {
         $Password = $this->put('Password');
         $ID = $this->put('ID');
 
         $result = $this->user->update($ID, $Password);
-        if($result === FALSE) {
+        if ($result === FALSE) {
             $this->response(array('status' => 'failed'));
         } else {
             $this->response(array('status' => 'success'));
         }
-
     }
 
 
@@ -87,48 +95,48 @@ class WebsiteRestController extends REST_Controller {
     // 	$users = 
     // }
 
- //    function website_get() {
- //        if (!$this->get('id')) { //query parameter, example, websites?id=1
- //            $this->response(NULL, 400);
- //        }
- //        $website = $this->wm->get_website($this->get('id'));
- //        if ($website) {
- //            $this->response($website, 200); // 200 being the HTTP response code
- //        } else {
- //            $this->response(array(), 500);
- //        }
- //    }
-	
-	// function add_website_post() {
- //        $website_title = $this->post('title');
- //        $website_url = $this->post('url');
-        
- //        $result = $this->wm->add_website($website_title, $website_url);
- //        if ($result === FALSE) {
- //            $this->response(array('status' => 'failed'));
- //        } else {
- //            $this->response(array('status' => 'success'));
- //        }
- //    }
- //    function update_website_put() {
- //        $website_id = $this->put('id');
- //        $website_title = $this->put('title');
- //        $website_url = $this->put('url');
- //        $result = $this->wm->update_website($website_id, $website_title, $website_url);
- //        if ($result === FALSE) {
- //            $this->response(array('status' => 'failed'));
- //        } else {
- //            $this->response(array('status' => 'success'));
- //        }
- //    }
-	
-	// function delete_website_delete($website_id) { //path parameter, example, /delete/1
- //        $result = $this->wm->delete_website($website_id);
- //        if ($result === FALSE) {
- //            $this->response(array('status' => 'failed'));
- //        } else {
- //            $this->response(array('status' => 'success'));
- //        }
- //    }
-	
+    //    function website_get() {
+    //        if (!$this->get('id')) { //query parameter, example, websites?id=1
+    //            $this->response(NULL, 400);
+    //        }
+    //        $website = $this->wm->get_website($this->get('id'));
+    //        if ($website) {
+    //            $this->response($website, 200); // 200 being the HTTP response code
+    //        } else {
+    //            $this->response(array(), 500);
+    //        }
+    //    }
+
+    // function add_website_post() {
+    //        $website_title = $this->post('title');
+    //        $website_url = $this->post('url');
+
+    //        $result = $this->wm->add_website($website_title, $website_url);
+    //        if ($result === FALSE) {
+    //            $this->response(array('status' => 'failed'));
+    //        } else {
+    //            $this->response(array('status' => 'success'));
+    //        }
+    //    }
+    //    function update_website_put() {
+    //        $website_id = $this->put('id');
+    //        $website_title = $this->put('title');
+    //        $website_url = $this->put('url');
+    //        $result = $this->wm->update_website($website_id, $website_title, $website_url);
+    //        if ($result === FALSE) {
+    //            $this->response(array('status' => 'failed'));
+    //        } else {
+    //            $this->response(array('status' => 'success'));
+    //        }
+    //    }
+
+    // function delete_website_delete($website_id) { //path parameter, example, /delete/1
+    //        $result = $this->wm->delete_website($website_id);
+    //        if ($result === FALSE) {
+    //            $this->response(array('status' => 'failed'));
+    //        } else {
+    //            $this->response(array('status' => 'success'));
+    //        }
+    //    }
+
 }
