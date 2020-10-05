b4bcf18dbeab08fedf07027882761b93d907fa41
opankaje <opankaje@gmail.com>
2020-10-05 18:30:50 +0700

    Update PengabdianMasyarakat.js
    Sudah jalan functionnya

--- src/components/pages/pengabdianMasyarakat/PengabdianMasyarakat.js
+++ src/components/pages/pengabdianMasyarakat/PengabdianMasyarakat.js
@@ -1,104 +1,256 @@
-import React, { Component } from 'react';
-import { Table, Col, Row } from 'reactstrap';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React, { Component, useState, useEffect } from "react";
+import { Table, Col, Row } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { Chart } from "react-google-charts";
-import { Container } from 'reactstrap';
+import { Container } from "reactstrap";
+import "./PengabdianMasyarakat.css";
+import { Input, FormGroup } from "reactstrap";
 import axios from "axios";
-import { Input, FormGroup } from 'reactstrap';
-import './PengabdianMasyarakat.css';
+import DragDrop from "../TarikBuang";
 
-class PengabdianMasyarakat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+function PengabdianMasyarakat (props) {
+  const [state,setState] = useState({
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+      tabel7a: [],
       tabel7: [],
-      tabel7Filtered: [],
+      tabelBukti7a: [],
       modal: false,
       dad: false,
       modalBukti: false,
       unggahBukti: false,
-    };
-    this.toggleModal = this.toggleModal.bind(this);
-  }
+    // };
+  })
+    // this.toggleModal = this.toggleModal.bind(this);
+    // this.toggleModalBukti = this.toggleModalBukti.bind(this);
+    // this.toggleModalUnggahBukti = this.toggleModalUnggahBukti.bind(this);
+  // }
 
-  toggleModal() {
-    if (this.state.modal == true) {
-      this.setState({ modal: false });
-    } else {
-      this.setState({ modal: true });
-    }
-  }
-
-  componentDidMount() {
-    axios.get('website-akreditasi-front-end/index.php/api/tabel7').then(data => {
-      this.setState({ tabel7: data.data.result, tabel7Filtered: data.data.result });
-    })
-  }
-
-  update(e) {
-    var x = this.state.tabel3a;
+  const updateA = (e) => {
+     var x = state.tabel3a;
+    // var x = this.state.tabel3a;
     let searchQuery = e.target.value;
     let regexer = new RegExp(searchQuery, "i");
-    this.setState({
-      tabel7Filtered: this.state.tabel7.filter(d => searchQuery.length == 0 || d.namaDosen.match(regexer))
+    setState({
+    ...state,
+    // this.setState({
+      tabel7: this.state.tabel7a.filter(
+        (d) => searchQuery.length == 0 || d.namaDosen.match(regexer)
+      ),
     });
   }
 
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_DROP_DEPTH":
+        return { ...state, dropDepth: action.dropDepth };
+      case "SET_IN_DROP_ZONE":
+        return { ...state, inDropZone: action.inDropZone };
+      case "ADD_FILE_TO_LIST":
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
 
-  render() {
-    const { tabel7, tabel7Filtered } = this.state;
+  const [data, dispatch] = React.useReducer(reducer, {
+    dropDepth: 0,
+    inDropZone: false,
+    fileList: [],
+  });
+
+  const toggleModal = () => {
+    // if (this.state.modal == true) {
+    //   this.setState({ modal: false });
+    // } else {
+    //   this.setState({ modal: true });
+    // }
+    if (state.modal == true) {
+      setState({ 
+        ...state,
+        modal: false });
+    } else {
+      setState({ 
+        ...state,
+        modal: true });
+    }
+  }
+
+  const toggleModalBukti = () => {
+    // if (this.state.modalBukti == true) {
+    //   this.setState({ modalBukti: false });
+    // } else {
+    //   this.setState({ modalBukti: true });
+    // }
+    if (state.modalBukti == true) {
+      setState({ 
+        ...state,
+        modalBukti: false });
+    } else {
+      setState({ 
+        ...state,
+        modalBukti: true });
+    }
+  }
+
+  const toggleModalUnggahBukti = () => {
+    // if (this.state.unggahBukti == true) {
+    //   this.setState({ unggahBukti: false});
+    // } else {
+    //   this.setState({ unggahBukti: true});
+    // }
+    if (state.unggahBukti == true) {
+      setState({ 
+        ...state,
+        unggahBukti: false});
+    } else {
+      setState({ 
+        ...state,
+        unggahBukti: true});
+    }
+  }
+
+
+  useEffect(() => {
+    axios
+    .get("website-akreditasi-front-end/index.php/api/tabel7a")
+    .then((data) => {
+      setState({ 
+        ...state,
+        tabel7a: data.data.result, tabel6: data.data.result });
+    }).catch((error) => {});
+    // console.log(state.tabel6);
+  }, []);
+
+
+  // componentDidMount() {
+  //   axios
+  //     .get("website-akreditasi-front-end/index.php/api/tabel6a")
+  //     .then((data) => {
+  //       this.setState({ tabel6a: data.data.result, tabel6: data.data.result });
+  //     });
+  //   console.log(this.state.tabel6);
+  // }
+
+  // render() {
+  
+    // const { tabel6a, tabel6 } = this.state;
+    // const { tabel6a, tabel6 } = state;
     let jml2016 = 0;
     let jml2017 = 0;
     let jml2018 = 0;
     let jml2019 = 0;
     let jml2020 = 0;
-    let tabel = tabel7Filtered.map((d, i) => {
-      if (d.tahun.substr(0, 4) == "2016") {
+    // console.log(state);
+    let tabel_6_a = state.tabel7.map((d, i) => {
+      if (d.tahun == 2016) {
         jml2016++;
-      } else if (d.tahun.substr(0, 4) == "2017") {
+      } else if (d.tahun == 2017) {
         jml2017++;
-      } else if (d.tahun.substr(0, 4) == "2018") {
+      } else if (d.tahun == 2018) {
         jml2018++;
-      } else if (d.tahun.substr(0, 4) == "2019") {
+      } else if (d.tahun == 2019) {
         jml2019++;
       } else {
         jml2020++;
       }
+      return (
+        <tr>
+          <td>{i + 1}</td>
+          <td style={{ textAlign: "left", width: 250 }}>{d.namaDosen}</td>
+          <td>{d.temaPenelitian}</td>
+          <td>{d.namaMahasiswa}</td>
+          <td className="text-left">{d.judulKegiatan}</td>
+          <td>{d.tahun}</td>
+          <td>
+          <Button
+            color="primary"
+            onClick={() => {
 
-      return <tr>
-        <td style={{ width: 20 }}>{i + 1}</td>
-        <td style={{ textAlign: 'left', width: 350 }}>{d.namaDosen}</td>
-        <td style={{ width: 100 }}>{d.temaPKM}</td>
-        <td style={{ textAlign: 'left', width: 250 }}>{d.namaMahasiswa}</td>
-        <td style={{ textAlign: 'left', width: 100 }}>{d.judulKegiatan}</td>
-        <td style={{ width: 35 }}>{d.tahun}</td>
-      </tr>
+              axios.get('demo-prosi-backend/index.php/C_Tabel6/getBukti/'+d.idPenelitian)
+                .then((data)=>{
+                  this.setState({ tabelBukti7a:data.data.result });
+                  // console.log(this.state.tabelBukti6a);
+                })
+              this.setState({
+                modalBukti: true,
+              });
+            }}
+          >
+            Lihat Bukti
+          </Button>
+          </td>
+        </tr>
+      );
     });
+
+    // if(this.state.tabelBukti6a!=null){
+      // let tabel_bukti_6_a = this.state.tabelBukti6a.map((d, i) => {
+        let tabel_bukti_6_a = state.tabelBukti7a.map((d, i) => {
+        // if(this.state.tabelBukti6a!=null){
+          if(state.tabelBukti7a!=null){
+          return (
+            <tr>
+              <td>{i + 1}</td>
+              <td style={{ textAlign: "left", width: 250 }}>{d.deskripsi}</td>
+              <td>
+                <a href={d.pathFile} target="_blank">{d.namaBukti}</a>  
+              </td>
+            </tr>
+          )
+        } else {
+          return (
+            <h4>Belum ada bukti</h4>
+          )
+        }
+      })
 
     return (
       <>
         <Container>
-          <h3 className="text-black font-weight-light my-5 text-center">Tabel 7 PkM DTPS yang melibatkan mahasiswa </h3>
+          <h3 className="text-black font-weight-light my-5 text-center">
+            Tabel 7 PkM DTPS yang melibatkan mahasiswa{" "}
+          </h3>
         </Container>
         <Container fluid="true">
           <div className="px-4">
             <Row>
               <Col className="mb-1">
-                <Button color="primary" className="grafik" onClick={() => {
-                  this.setState({
-                    modal: true
-                  });
-                }}>Grafik</Button>
-                 <Button color="primary" 
+                <Button
+                  color="primary"
+                  className="grafik"
+                  onClick={() => {
+                    // this.setState({
+                    setState({
+                      ...state,
+                      modal: true,
+                    });
+                  }}
+                >
+                  Grafik
+                </Button>
+                <Button color="primary" 
                 className="unggahBukti"
-                
+                onClick={() => {
+                  // this.setState({
+                  setState({
+                    ...state,
+                    unggahBukti: true,
+                  });
+                }}
                 >
                   Unggah Bukti
                 </Button>
               </Col>
               <Col md={3} className="ml-auto">
                 <FormGroup className="input">
-                  <Input type="text" onChange={this.update.bind(this)} placeholder="Cari Dosen" />
+                  <Input
+                    type="text"
+                    // onChange={this.updateA.bind(this)}
+                    onChange={updateA}
+                    placeholder="Cari Dosen"
+                  />
                 </FormGroup>
               </Col>
             </Row>
@@ -107,19 +259,46 @@ class PengabdianMasyarakat extends Component {
                 <Table striped bordered responsive className="text-center">
                   <thead>
                     <tr>
-                      <th className="align-middle">No.</th>
+                      <th style={{ width: 10 }} className="align-middle">
+                        No.
+                      </th>
                       <th className="align-middle">Nama Dosen</th>
-                      <th className="align-middle">Tema PkM Sesuai Roadmap</th>
+                      <th className="align-middle">
+                        Tema PKM Sesuai Roadmap
+                      </th>
                       <th className="align-middle">Nama Mahasiswa</th>
                       <th className="align-middle">Judul Kegiatan</th>
                       <th className="align-middle">Tahun</th>
+                      <th className="align-middle">Bukti</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {tabel}
+                    {tabel_6_a}
                     <tr>
-                      <td colSpan="5">Jumlah</td>
-                      <td>{tabel.length}</td>
+                      <td>1</td>
+                      <td>Elisati Hulu</td>
+                      <td>Jakarta</td>
+                      <td>Naufal</td>
+                      <td>Smartmirror menggunakan Arduino</td>
+                      <td>2018</td>
+                      <td>
+                        <Button
+                          color="primary"
+                          onClick={() => {
+                            // this.setState({
+                              setState({
+                              ...state,
+                              modalBukti: true,
+                            });
+                          }}
+                        >
+                          Lihat Bukti
+                        </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td colSpan="6">Jumlah </td>
+                      <td>{tabel_6_a.length}</td>
                     </tr>
                   </tbody>
                 </Table>
@@ -129,40 +308,107 @@ class PengabdianMasyarakat extends Component {
         </Container>
 
         <div>
-          <Modal size={'xl'} isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
-            <ModalHeader toggle={this.toggleModal}>Grafik Tahun PkM DTPS yang Melibatkan Mahasiswa</ModalHeader>
+          <Modal
+            size={"xl"}
+            // isOpen={this.state.modal}
+            // toggle={this.toggleModal}
+            // className={this.props.className}
+            isOpen={state.modal}
+            toggle={toggleModal}
+            className={props.className}
+          >
+            {/* <ModalHeader toggle={this.toggleModal}> */}
+            <ModalHeader toggle={toggleModal}>
+              Grafik PkM yang Melibatkan Mahasiswa
+            </ModalHeader>
             <ModalBody>
               <Container>
-                <Col md={12} style={{ float: 'left' }}>
+                <Col md={12} style={{ float: "left" }}>
                   <Chart
-                    width={'1000px'}
-                    height={'450px'}
+                    width={"1000px"}
+                    height={"450px"}
                     chartType="PieChart"
                     loader={<div>Loading Chart</div>}
                     data={[
-                      ['Tahun', 'Jumlah'],
-                      ['2016', jml2016],
-                      ['2017', jml2017],
-                      ['2018', jml2018],
-                      ['2019', jml2019],
-                      ['2020', jml2020],
+                      ["Tahun", "Jumlah"],
+                      ["2016", jml2016],
+                      ["2017", jml2017],
+                      ["2018", jml2018],
+                      ["2019", jml2019],
+                      ["2020", jml2020],
                     ]}
                     options={{
-                      title: 'PkM DTPS yang Melibatkan Mahasiswa',
+                      title: "PkM yang Melibatkan Mahasiswa",
                       is3D: false,
                     }}
-                    rootProps={{ 'data-testid': '2' }}
+                    rootProps={{ "data-testid": "2" }}
                   />
                 </Col>
               </Container>
             </ModalBody>
           </Modal>
-        </div >
+          <Modal
+            size={"xl"}
+            // isOpen={this.state.modalBukti}
+            // toggle={this.toggleModalBukti}
+            // className={this.props.className}
+            isOpen={state.modalBukti}
+            toggle={toggleModalBukti}
+            className={props.className}
+          >
+            {/* <ModalHeader toggle={this.toggleModalBukti}> */}
+            <ModalHeader toggle={toggleModalBukti}>
+              Bukti
+            </ModalHeader>
+            <ModalBody>
+              <Container>
+                <Table striped bordered responsive className="text-center">
+                  <thead>
+                    <tr>
+                      <th style={{ width: 10 }} className="align-middle">
+                        No.
+                      </th>
+                      <th className="align-middle">Nama Bukti</th>
+                      <th className="align-middle">
+                        Dokumen Bukti
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* <tr>
+                      <td>1</td>
+                      <td>Sertifikat Smart Mirror menggunakan Arduiono</td>
+                      <td><a href="facebook.com">Sertifikat.pdf</a></td>
+                    </tr> */}
+                    {tabel_bukti_6_a}
+                  </tbody>
+                </Table>
+              </Container>
+            </ModalBody>
+          </Modal>
+
+          <Modal
+            size={"xl"}
+            // isOpen={this.state.unggahBukti}
+            // toggle={this.toggleModalUnggahlBukti}
+            // className={this.props.className}
+            isOpen={state.unggahBukti}
+            toggle={toggleModalUnggahBukti}
+            className={props.className}
+          >
+            {/* <ModalHeader toggle={this.toggleModalUnggahBukti}> */}
+            <ModalHeader toggle={toggleModalUnggahBukti}>
+              Bukti
+            </ModalHeader>
+            <ModalBody>
+              <DragDrop data={data} dispatch={dispatch}/>
+            </ModalBody>
+          </Modal>
+        </div>
       </>
-    )
-  }
+    );
+  // }
 }
-PengabdianMasyarakat.propTypes = {
-}
+// Mahasiswa.propTypes = {};
 
 export default PengabdianMasyarakat;
